85e4bc95bd61f1a4a3dc21870eda6487bf3fab0f
Jackie Xu <jackiexu2007@hotmail.com>
2020-03-23 13:02:25 -0400

    Adding inline commands support

--- handler.py
+++ handler.py
@@ -13,6 +13,11 @@ BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 def trigger(event, context):
 
     data = json.loads(event["body"])
+
+    if "inline_query" in data:
+        return handle_inline_query(data)
+
+
     message = str(data["message"]["text"])
     chat_id = data["message"]["chat"]["id"]
     first_name = data["message"]["chat"]["first_name"]
@@ -26,6 +31,9 @@ def trigger(event, context):
     elif message.startswith("/joke"):
         r = requests.get("https://icanhazdadjoke.com/", headers={"Accept": "text/plain"})
         response = r.content.decode() if r.ok else "Oops! Can't fetch joke right now."
+    elif message.startswith("/fact"):
+        r = requests.get("https://www.mentalfloss.com/api/facts")
+        response = str(r.json()[0]['headline']).replace("<em>", "__").replace("</em>", "__").replace("<i>", "__").replace("</i>", "__") if r.ok else "Oops! Can't fetch fact right now."
     else:
         response = "Sorry, I don't understand. Try /help"
 
@@ -36,4 +44,27 @@ def trigger(event, context):
     url = BASE_URL + "/sendMessage"
     requests.post(url, data)
 
+    return {"statusCode": 200}
+
+def handle_inline_query(data):
+    query_id = data["inline_query"]["id"]
+    query = data["inline_query"]["query"]
+
+    # Dummy test data for now
+    pic = {
+        "type": "photo",
+        "id": "my_photo",
+        "photo_url": "https://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg",
+        "thumb_url": "https://upload.wikimedia.org/wikipedia/commons/2/22/Turkish_Van_Cat.jpg"
+    }
+
+    results = [
+        pic,
+    ]
+    data = {
+        'inline_query_id': query_id,
+        'results': results
+    }
+    url = BASE_URL + "/answerInlineQuery"
+    res = requests.post(url, json=data)
     return {"statusCode": 200}
