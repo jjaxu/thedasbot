301572c17aae52b0fa043ca61425306c3807ebb2
Jackie Xu <jackiexu2007@hotmail.com>
2020-04-15 12:31:35 -0400

    Fixing UT - intentional to observe CI

--- test/trivia_command_test.py
+++ test/trivia_command_test.py
@@ -2,4 +2,4 @@ import unittest, setup
 
 class TrivaTest(unittest.TestCase):
     def test_test(self):
-        self.assertEqual(True, False, "Wrong!")
+        self.assertEqual(True, True, "Wrong!")
