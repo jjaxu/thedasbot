86f4eceea3cfce1bcc9042d9b180fe130ef835d3
Jackie Xu <jackiexu2007@hotmail.com>
2022-04-15 13:22:46 -0400

    Update facts fetch, the API url changed

--- commands/fact_command.py
+++ commands/fact_command.py
@@ -1,20 +1,25 @@
-import requests
+import requests, traceback
 
 from constants import PARSE_MODE_KEY
 from .botcommand import BotCommand
 from boterror import BotError
 from botquery import BotQuery
 
+FACTS_URL = "https://facts-service.mmsport.voltaxservices.io/widget/properties/mentalfloss/random-facts"
+
 class FactCommand(BotCommand):
     def execute(self):
         result = None
         try:
-            response = requests.get("https://www.mentalfloss.com/api/facts", headers={"Accept": "text/plain"})
+            response = requests.get(FACTS_URL)
             if response.ok:
-                fact = response.json()[0]
-                result = f'<a href="{fact["primaryImage"]}">&#x200b;</a>{str(fact["headline"])}'
-        except Exception as err:
-            print(f"Unable to fetch fact:\n\t{str(err)}")
+                res = response.json()["data"][0]
+                fact = res["body"]
+                print(res)
+                image_url = res["image"]["value"]["url"]
+                result = f'<a href="{image_url}">&#x200b;</a>{str(fact)}'
+        except Exception:
+            print(f"Unable to fetch fact:\n\t{traceback.format_exc()}")
 
         self.handle_normal_query(result or "Oops! Can't fetch fact right now.")
     
